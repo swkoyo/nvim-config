9e0e0e254121d1c96069e62d32a2f80326336e12
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-07-17 18:50:55 +0900

    update

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,3 +1,2 @@
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
-)
+require('trouble').setup()
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
