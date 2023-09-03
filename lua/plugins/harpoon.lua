48b8af0322dfb209df54d04c69083777ee24af9d
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-09-03 15:26:52 +0900

    Add flash telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,12 +2,12 @@ return {
 	"ThePrimeagen/harpoon",
 	-- stylua: ignore
 	keys = {
-		{ "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end },
-		{ "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon add" },
-		{ "<C-1>", function() require("harpoon.ui").nav_file(1) end },
-		{ "<C-2>", function() require("harpoon.ui").nav_file(2) end },
-		{ "<C-3>", function() require("harpoon.ui").nav_file(3) end },
-		{ "<C-4>", function() require("harpoon.ui").nav_file(4) end },
+		{ "<leader>hm", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon menu" },
+		{ "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Harpoon add" },
+		{ "<leader>h1", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon file 1" },
+		{ "<leader>h2", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon file 2" },
+		{ "<leader>h3", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon file 3" },
+		{ "<leader>h4", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon file 4" },
 	},
 	config = true,
 }
