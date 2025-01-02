a3a6fdd4b3cc2ca55ee25b4eb4fbf58743534582
swkoyo <10441291+swkoyo@users.noreply.github.com>
2025-01-02 11:36:12 +0900

    nvim gruvbox

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,17 +1,31 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		style = "night",
-		transparent = true,
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			transparent_mode = true,
 		},
+		config = function(_, opts)
+			require("gruvbox").setup(opts)
+			vim.cmd.colorscheme("gruvbox")
+		end,
 	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
-		vim.cmd.colorscheme("tokyonight")
-	end,
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		style = "night",
+	-- 		transparent = true,
+	-- 		styles = {
+	-- 			sidebars = "transparent",
+	-- 			floats = "transparent",
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("tokyonight").setup(opts)
+	-- 		vim.cmd.colorscheme("tokyonight")
+	-- 	end,
+	-- },
 }
